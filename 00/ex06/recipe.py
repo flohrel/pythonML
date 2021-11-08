2e03d74f26a13ada7a2dca99eeb399dc60ccba11
flohrel <flohrel@e3r3p21.clusters.42paris.fr>
2021-11-08 18:39:16 +0100

    00/ex06 OK

--- 00/ex06/recipe.py
+++ 00/ex06/recipe.py
@@ -16,18 +16,28 @@ cookbook = {
     }
 }
 
-def getRecipe():
-    print("Please enter the recipe's name to get its details:")
-    arg = input(">> ")
-    recipe = cookbook.get(arg)
-    if not recipe:
-        print(arg, "not found")
-        return
-    print("Recipe for", arg + ":")
-    print("Ingredients list", recipe.get("ingredients"))
+def getRecipe(recipe, arg):
+    print("\nRecipe for", arg + ":")
+    print("Ingredients list:", recipe.get("ingredients"))
     print("To be eaten for", recipe.get("meal"))
     print("Takes", recipe.get("prep_time"), "minutes of cooking")
+
+def addRecipe():
+    new = dict.fromkeys(("ingredients", "meal", "prep_time"), 0)
+    print("\nPlease enter recipe name to add it to cookbook:")
+    recipe = input(">> ")
+    print("List of ingredients:")
+    var = input(">> ")
+    new.update({"ingredients": var})
+    print("Type of meal:")
+    var = input(">> ")
+    new.update({"meal": var})
+    print("Preparation time:")
+    var = input(">> ")
+    new.update({"prep_time": var})
+    cookbook.update({recipe: new})
     
+
 while 1:
     print("\nPlease select an option by typing the corresponding number:")
     print("1: Add a recipe")
@@ -38,10 +48,31 @@ while 1:
     try:
         index = int(input(">> "))
     except:
-        print("\nInvalid argument")
+        print("\nThis option does not exist, please type the corresponding number.")
     else:
         if index < 1 or index > 5:
-            print("\nInvalid option")
+            print("\nThis option does not exist, please type the corresponding number.")
+        elif index == 1:
+            addRecipe()
+        elif index == 2:
+            print("\nPlease enter recipe name to delete it:")
+            arg = input(">> ")
+            recipe = cookbook.get(arg)
+            if not recipe:
+                print("\n", arg, "not found")
+            else:
+                cookbook.pop(arg)
+        elif index == 3:
+            print("\nPlease enter the recipe's name to get its details:")
+            arg = input(">> ")
+            recipe = cookbook.get(arg)
+            if not recipe:
+                print("\n", arg, "not found")
+            else:
+                getRecipe(recipe, arg)
+        elif index == 4:
+            for key in cookbook.keys():
+                getRecipe(cookbook.get(key), key)
         elif index == 5:
             print("\nCookbook closed")
             exit()
