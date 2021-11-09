0b39df99e22dd7bdf2b2b9733b2b94aacc40cc52
flohrel <flohrel@student.42.fr>
2021-11-09 14:00:49 +0100

    00/ex10 OK

---
+++ 00/ex10/loading.py
@@ -0,0 +1,26 @@
+import time
+
+def ft_progress(listy):
+    bar = ">"
+    start = time.perf_counter()
+    total = len(listy)
+    for i in listy:
+        t = time.perf_counter() - start
+        i += 1
+        eta = (total * t) / i
+        percentage = int((i * 100) / total)
+        progress = int((percentage * 20) / 100)
+        if progress > len(bar):
+            bar = bar.strip(">")
+            bar += "=>"
+        line = "ETA:{0:.2f}s [{1: >3d}%] [{2:<20}] {3}/{4} | elapsed time {5:.2f}s"
+        print(line.format(eta, percentage, bar, i, total, t), end="\r")
+        yield i
+
+listy = range(1000)
+ret = 0
+for elem in ft_progress(listy):
+    ret += (elem + 3) % 5
+    time.sleep(0.01)
+print()
+print(ret)
